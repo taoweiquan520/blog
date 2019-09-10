50cd7622d2f538591b9946690110a5e508e654c5
taoweiquan520 <347499586@qq.com>
2019-09-10 11:27:19 +0800

    sidebar优化

--- app/components/content/Sidebar.js
+++ app/components/content/Sidebar.js
@@ -1,41 +1,54 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getArticleList } from '../../actions/articleAction';
 import './css/sidebar.css'
 
 class Sidebar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.categoryChange = this.categoryChange.bind(this);
+    }
+
+    categoryChange(url) {
+        console.log(this.context)
+        const {dispatch} = this.props;
+        
+        dispatch(getArticleList.asyncAction(url));
+        this.context.router.push(`/category/${url}`);
+    }
+
+    componentDidMount() {
+        
+    }
+    componentDidUpdate() {
+        alert('1')
+
+    }
+
     render() {
+        const list = ['html', 'javascript', 'css', 'react', 'redux'];
         return (
             <div className="side">
                 <div className="side-toolbar">
-                    <div className="toolbar-item">
-                        <NavLink to="/category/html" activeClassName="selected">
-                            <b>1.</b>html
-                        </NavLink>
-                    </div>
-                    <div className="toolbar-item">
-                        <NavLink to="/category/javascript" activeClassName="selected">
-                            <b>2.</b>javascript
-                        </NavLink>
-                    </div>
-                    <div className="toolbar-item">
-                        <NavLink to="/category/css" activeClassName="selected">
-                            <b>3.</b>css
-                        </NavLink>
-                    </div>
-                    <div className="toolbar-item">
-                        <NavLink to="/category/react" activeClassName="selected">
-                            <b>4.</b>react
-                        </NavLink>
-                    </div>
-                    <div className="toolbar-item">
-                        <NavLink to="/category/redux" activeClassName="selected">
-                            <b>5.</b>redux
-                        </NavLink>
-                    </div>
-                </div>
+                {
+                    list.map((item, index) => (
+                        <div className="toolbar-item" key={index}>
+                            <a onClick={e => this.categoryChange(item)}>
+                                <b>{index + 1}.</b>{item}
+                            </a>
+                        </div>
+                    ))
+                }
+                </div>  
             </div>
         )
     }
 }
+const mapStateToProps = (state, ownProps) => {
+    return state.articleReducer;
+};
 
-export default Sidebar;
+export default connect(
+    mapStateToProps
+)(Sidebar);
